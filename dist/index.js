38a94d1a8efa2e3d3596fe4705f3d7dd69a1d16e
xiaochan <xiaochan@bytedance.com>
2019-02-21 14:32:02 +0800

    feat: view -> dom 实现

--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,52 @@
+function flatten(arr) {
+  return [].concat(...arr);
+}
+
+function h(type, props, ...children) {
+  return {
+    type,
+    props: props || {},
+    children: flatten(children || [])
+  };
+}
+
+function createElement(node) {
+  if (typeof node === 'string') {
+    return document.createTextNode(node);
+  } else {
+    const { type, props, children } = node;
+    const el = document.createElement(type);
+    setProps(el, props);
+    if (Array.isArray(children)) {
+      children.map(child => {
+        el.appendChild(createElement(child));
+      });
+    }
+    return el;
+  }
+}
+
+function setProps(el, props) {
+  for (const key in props) {
+    el.setAttribute(key, props[key]);
+  }
+}
+
 function view() {
-  return rh(
+  return h(
     "ul",
     { id: "filmList", className: "list" },
-    rh(
+    h(
       "li",
       { className: "main" },
       "Detective Chinatown Vol 2"
     ),
-    rh(
+    h(
       "li",
       null,
       "Ferdinand"
     ),
-    rh(
+    h(
       "li",
       null,
       "Paddington 2"
@@ -20,6 +54,7 @@ function view() {
   );
 }
 
-function render() {
-  console.log(view());
+function render(container) {
+  const dom = createElement(view());
+  container.appendChild(dom);
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,48 @@
-function view() {
-    return <ul id="filmList" className="list">
+function flatten(arr) {
+  return [].concat(...arr);
+}
+
+function h(type, props, ...children) {
+  return {
+    type,
+    props: props || {},
+    children: flatten(children || [])
+  };
+}
+
+function createElement (node) {
+  if (typeof node === 'string') {
+    return document.createTextNode(node);
+  } else {
+    const { type, props, children } = node;
+    const el = document.createElement(type);
+    setProps(el, props);
+    if (Array.isArray(children)) {
+      children.map(child => {
+        el.appendChild(createElement(child));
+      });
+    }
+    return el;
+  }
+}
+
+function setProps (el, props) {
+  for (const key in props) {
+    el.setAttribute(key, props[key]);
+  }
+}
+
+function view () {
+  return (
+    <ul id="filmList" className="list">
       <li className="main">Detective Chinatown Vol 2</li>
       <li>Ferdinand</li>
       <li>Paddington 2</li>
     </ul>
-  }
+  )
+}
 
-function render() {
-    console.log(view())
+function render(container) {
+  const dom = createElement(view());
+  container.appendChild(dom);
 }
